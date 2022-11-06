48dc3d47d5c05c02e50fdd39cf89b240fd8ae53f
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-06 11:13:37 +0100

    eerste commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const batmanJson = require("./batman.json");
 
 function App() {
+  const bm = batmanJson.Search.map((batman) => {
+    return (
+      <div>
+        <Batman batman={batman}></Batman>
+      </div>
+    );
+  });
+  return <div>{bm}</div>;
+}
+
+//component of batman ussing prop
+const Batman = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='batman_properties'>
+      <div>
+        <span className='title'>{props.batman.Title}</span>
+      </div>
+      <div>
+        <span className='year'>{props.batman.Year}</span>
+      </div>
+      <div>
+        <span>{props.batman.imdbID}</span>
+      </div>
+      <div>
+        <span>{props.batman.Type}</span>
+      </div>
+      <div>
+        <img src={props.batman.Poster} alt={props.batman.Title}/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
